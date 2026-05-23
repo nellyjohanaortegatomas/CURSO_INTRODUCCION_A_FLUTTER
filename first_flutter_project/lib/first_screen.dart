18487700dc68d547a6cd12c083001711d8cb7cb4
nellyjohanaortegatomas <nellyjohanaortega53@gmail.com>
2026-05-23 11:31:22 -0600

    Seccion 6: Working with Forms - Clase 13: Form Elements

--- first_flutter_project/lib/first_screen.dart
+++ first_flutter_project/lib/first_screen.dart
@@ -1,45 +1,125 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
-class FirstClass extends StatelessWidget{
+class SecondClass extends StatefulWidget {
+  @override
+  _SecondClassState createState() => _SecondClassState();
+}
+
+class _SecondClassState extends State<SecondClass> {
+  String maritalStatus = 'single';
+  bool termsChecked = true;
+
+  List<String> locations = ['Siglo XXI', 'Casas Geo', 'Loma Alta', 'Camelia'];
+  String? selectedLocation;
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-        body: Material(
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.ac_unit),
-              title: Text('Perro'),
-              subtitle: Text('Esto es un animal.'),
-              trailing: Icon(Icons.access_time),
-            ),
-            ListTile(
-              leading: Icon(Icons.access_alarm),
-              title: Text('Gato'),
-              subtitle: Text('Esto es un animal.'),
-              trailing: Icon(Icons.access_time),
-            ),
-            Padding(
-              child: Text('Perro'),
-              padding: EdgeInsets.all(10.0),
-            ),
-            Container(
-              child: Text('Gato'),
-              margin: EdgeInsets.symmetric(horizontal: 30.0),
-              color: Colors.deepPurple,
-              padding: EdgeInsets.only(top: 20.0),
-              )
-
+        appBar: AppBar(
+          elevation: 10.0,
+          //title: Center(child: Text('Title')
+          //),
+          title: Text('Title'),
+          centerTitle: true,
+          actions: <Widget>[
+            Icon(Icons.settings),
           ],
-        )
-     ) );
-  }
+          /*bottom: PreferredSize(
+            preferredSize: Size.fromHeight(40.0),
+            child: Text('Este es un texto en la appbar'),
+          ),*/
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Form(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  maxLength: 20,
+                  decoration: InputDecoration(hintText: 'Nombre', labelText: 'Ingresa tu Nombre'),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Edad', labelText: 'Ingresa tu Edad'),
+                  keyboardType: TextInputType.phone,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(hintText: 'Contraseña', labelText: 'Ingresa tu Contraseña'),
+                ),
+                DropdownButton<String>(
+                  hint: Text('Por favor, elige la colonia en la que vives.'),
+                  value: selectedLocation,
+                  items: locations.map((location) {
+                    return DropdownMenuItem(
+                      child: Text(location),
+                      value: location,
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedLocation = value!;
+                    });
+                  },
+                ),
 
-  String generateNumbers() {
-    var r=Random();
-    int i=r.nextInt(22);
-    return 'Un número aleatorio entre 0 y 22 es ${i}';
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Expanded(
+                      child: RadioListTile<String>(
+                        title: const Text('Soltero/a'),
+                        value: 'single',
+                        groupValue: maritalStatus,
+                        onChanged: (value) {
+                          setState(() {
+                            maritalStatus = value!;
+                          });
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: RadioListTile<String>(
+                        title: const Text('Casado/a'),
+                        value: 'married',
+                        groupValue: maritalStatus,
+                        onChanged: (value) {
+                          setState(() {
+                            maritalStatus = value!;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+
+                CheckboxListTile(
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: termsChecked,
+                  title: new Text(
+                    'Suscribirse al periódico y a los artículos relacionados.',
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      termsChecked = value!;
+                    });
+                  },
+                ),
+
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {},
+                  child: Text('Registrar'),
+                )
+              ],
+            )
+          ),
+        )
+      )
+    );
   }
 }
--- first_flutter_project/lib/main.dart
+++ first_flutter_project/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:first_flutter_project/first_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp( Myclass() );
+void main() => runApp(Myclass());
 
 class Myclass extends StatelessWidget{
   @override
@@ -10,23 +10,7 @@ class Myclass extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "My flutter app",
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 10.0,
-          title: Center(child: Text('Título'),
-          ),
-          actions: <Widget>[
-            Icon(Icons.settings)
-          ],
-          /*
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(40.0),
-            child: Text('Este es un texto en la appbar'),
-          ),
-          */
-        ),
-        body: FirstClass()
-      )
-  );
+      home: SecondClass(),
+    );
   }
 }
