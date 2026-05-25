a0430e4d73e98e809092bb77d69a58898d86d0e6
nellyjohanaortegatomas <nellyjohanaortega53@gmail.com>
2026-05-25 05:59:37 -0600

    Sección 9: Earthquake App - Clase 25: Future Builder

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -26,6 +26,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  fetchPosts() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,70 +41,27 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(
-        child: ListView.builder(
-          itemCount: 15,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              decoration: BoxDecoration(
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    color: Color(0xFFE0E0E0),
-                    offset: Offset(0.5, 0.5),
-                    blurRadius: 10.0,
+        child: FutureBuilder(
+          future: fetchPosts(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            } else if (snapshot.data != null) {
+              if (snapshot.data == "Disculpen las molestias, el servidor está en mantenimiento.") {
+                return Container(
+                  child: Center(
+                    child: Text(snapshot.data),
                   ),
-                ],
-                shape: BoxShape.rectangle,
-                color: Color(0xFFFAFAFA),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.all(8),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width / 6,
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.purple,
-                      ),
-                      child: Container(
-                        margin: EdgeInsets.only(
-                          left: MediaQuery.of(context).size.width / 19,
-                        ),
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Banglore",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        Text(
-                          "15 Km from Banglore",
-                          style: TextStyle(fontSize: 12),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            );
+                );
+              } else {
+                return Container();
+              }
+            }
+            return Container();
           },
         ),
       ),
